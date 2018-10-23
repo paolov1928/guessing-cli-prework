88aee00b494daabc01f2bd450725fd25ad6552d1
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-23 18:16:13 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,17 @@
 # Code your solution here!
+require 'pry'
+def run_guessing_game
+#number = 1
+guess = ""
+  while guess != 'exit'
+    number = rand(1..6).to_s
+    puts "Guess a number between 1 and 6."
+    guess = gets.chomp
+      if guess == number
+        puts "You guessed the correct number!"
+      else
+        puts "The computer guessed #{number}."
+      end
+  end
+puts "Goodbye!"
+end
