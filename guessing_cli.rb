dfa4e288a99b931e95bfec4ff06db22298164f18
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-23 17:59:45 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,7 +7,8 @@ guess = ""
     number = rand(1..6).to_s
     puts "Guess a number between 1 and 6."
     guess = gets.chomp
-    break if guess == 'exit'
+      if guess == 'exit'
+        
     break if guess == number
     puts "The computer guessed #{number}"
     end
